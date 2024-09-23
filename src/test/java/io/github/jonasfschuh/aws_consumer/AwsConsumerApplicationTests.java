93abaa0d875da8d82ec29c3034cbe63cbfad229c
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-23 12:04:24 -0300

    Initial commit

---
+++ src/test/java/io/github/jonasfschuh/aws_consumer/AwsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jonasfschuh.aws_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
